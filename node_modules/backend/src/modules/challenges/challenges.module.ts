70f74101d934a5e3ff62d99ed66276211dcd102f
Muhammad Jiya <thecodedsurveyor@gmail.com>
2025-07-11 16:52:46 +0100

    Add .gitignore and remove node_modules from tracking

--- node_modules/backend/src/modules/challenges/challenges.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChallengesController } from './challenges.controller';
-import { ChallengesService } from './challenges.service';
-
-@Module({
-  controllers: [ChallengesController],
-  providers: [ChallengesService],
-})
-export class ChallengesModule {}
